0e66e5bac076361efd43a8c584758ab85890a0c7
unknown <thukano0405@i.softbank.jp>
2021-07-17 12:56:56 +0900

    Create edit.py

--- edit.py
+++ edit.py
@@ -5,17 +5,13 @@ import os
 #files=エクセルファイルが格納されている変数
 def edit(files):
     print('sono1')
-    print(files)
-    #データフレーム形式に変換
-    edit_df = pd.read_excel('static/csv')
-    print('sono2')
-    print(edit_df)
+    print(files[0])
     #パスを作成
-    edit_file_path = 'download/' + 'csv' + '_result.xlsx'
+    edit_file_path = 'download/' + 'test' + '_result.xlsx'
     print('sono3')
     print(edit_file_path)
     #作成したパスにエクセルファイルを保存
-    edit_df.to_excel(edit_file_path)
+    files[0].to_excel(edit_file_path)
     print('sono4')
     workbook = openpyxl.load_workbook(edit_file_path)
     print('sono5')
